df85929e7c6a37d5c7c69f72c6bfc3e04a76e739
serandres <sergiohurtadoramirez@gmail.com>
2014-09-14 21:58:37 -0500

    update

--- service.php
+++ service.php
@@ -1,3 +1,13 @@
 <?php 
-	
+	//http://localhost:8090/serviceaddress/service.php?lat=4.73886&lon=-75.90998
+	//http://maps.googleapis.com/maps/api/geocode/json?latlng=4.73886,-75.90998&sensor=true_or_false
+	//echo $_GET['lon'];
+	//echo $_GET['lat'];
+	$lonlat=$_GET['lat'].','.$_GET['lon'];
+	echo $lonlat;
+
 ?>
+	<script type="text/javascript">
+		var url='http://maps.googleapis.com/maps/api/geocode/json?latlng=$lonlat&sensor=true_or_false';
+		console.log(url);
+	</script>
