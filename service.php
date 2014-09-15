e0e61ce3c111c5814ea0bcd7a110b8da38aac012
Harold Sánchez <programador@gmys.com.co>
2014-09-15 11:57:30 -0500

    codigo final

--- service.php
+++ service.php
@@ -1,18 +1,10 @@
 <?php 
-	//http://localhost:8090/serviceaddress/service.php?lat=4.73886&lon=-75.90998
-	//http://maps.googleapis.com/maps/api/geocode/json?latlng=4.73886,-75.90998&sensor=true_or_false
-	//echo $_GET['lon'];
-	//echo $_GET['lat'];
+	header('Access-Control-Allow-Origin: *'); 
 	if(!$_GET['lon'] && !$_GET['lat']){
 		echo 'No hay datos para procesar!';
 	}else{
 		$lonlat=$_GET['lat'].','.$_GET['lon'];
-		echo $lonlat;
+		header ("Location: http://maps.googleapis.com/maps/api/geocode/json?latlng=".$lonlat."&sensor=true_or_false"); 
 
-?>
-		<script type="text/javascript">
-			var url='http://maps.googleapis.com/maps/api/geocode/json?latlng=<?php echo $lonlat; ?>&sensor=true_or_false';
-		</script>
-<?php
-}
+	}
 ?>
